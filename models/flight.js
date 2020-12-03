3f4aaac1552f0e24a2f69204bd40c6c21d7d7fe0
pohern <ohern_p@yahoo.com>
2020-12-02 22:05:29 -0600

    add min and max for flightNo

--- models/flight.js
+++ models/flight.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const flightSchema = new Schema({
     airline: {type:String, enum:['American', 'Southwest', 'United']},
     airport: {type:String, enum: ['DEN', 'AUS', 'LAX', 'SAN', 'DFW'], default: 'DEN'},
-    flightNo: Number,
+    flightNo: {type:Number, required: true, min: 10, max: 9999},
     departs: {type:Date, default:function(){
         return new (Date().getFullYear()+1)
     }}
