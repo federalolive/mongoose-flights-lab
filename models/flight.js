967b58e2c80390b17ad11535c5a06aed8e349f5f
pohern <ohern_p@yahoo.com>
2020-12-02 20:41:18 -0600

    add defaults

--- models/flight.js
+++ models/flight.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const flightSchema = new Schema({
-    airline: {type:String, enum: ['DEN', 'AUS', 'LAX', 'SAN', 'DFW'], default: 'DEN'},
-    airport: String,
+    airline: {type:String, enum:['American', 'Southwest', 'United']},
+    airport: {type:String, enum: ['DEN', 'AUS', 'LAX', 'SAN', 'DFW'], default: 'DEN'},
     flightNo: Number,
-    departs: Date
+    departs: {type:Date, default:function(){
+        return new (Date().getFullYear()+1)
+    }}
 })
 
 module.exports = mongoose.model('Flight', flightSchema)
