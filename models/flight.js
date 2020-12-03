92effb4f7b690e26c3a0988cd88f6f27d19233ed
pohern <ohern_p@yahoo.com>
2020-12-03 08:04:16 -0600

    fixing error with departs

--- models/flight.js
+++ models/flight.js
@@ -5,9 +5,11 @@ const flightSchema = new Schema({
     airline: {type:String, enum:['American', 'Southwest', 'United']},
     airport: {type:String, enum: ['DEN', 'AUS', 'LAX', 'SAN', 'DFW'], default: 'DEN'},
     flightNo: {type:Number, required: true, min: 10, max: 9999},
-    departs: {type:Date, default:function(){
-        return new Date().getFullYear()+1
-    }}
+    departs: {
+        type: Date, 
+        default: () => Date.now() + 365 * 24 * 60 * 60 * 1000 
+        // + 365 days * hours * minutes * seconds * milliseconds
+         }
 })
 
 module.exports = mongoose.model('Flight', flightSchema)
