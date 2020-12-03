4dbabe59e6d1c6a95d0c25d9176fd0a7426dc258
pohern <ohern_p@yahoo.com>
2020-12-02 21:08:48 -0600

    add create function

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,9 +1,21 @@
-   const Flight = require('../models/flight')
-   
-   module.exports = {
-        new: newFlight
-    }
+const Flight = require('../models/flight')
 
-    function newFlight(req, res) {
-        res.render('flights/new')
-    }
+module.exports = {
+    new: newFlight,
+    create
+}
+
+function newFlight(req, res) {
+    res.render('flights/new')
+}
+
+function create(req, res){
+    const flight = new Flight(req.body)
+    flight.save(function(err) {
+    if (err) return res.render('flights/new')
+    console.log(flight)
+    res.redirect('/flights')
+  })
+    res.render('/flights')
+}
+
--- routes/flights.js
+++ routes/flights.js
@@ -3,5 +3,6 @@ var router = express.Router();
 const flightsCtrl = require('../controllers/flights')
 
 router.get('/new', flightsCtrl.new)
+router.post('/', flightsCtrl.create)
 
 module.exports = router;
