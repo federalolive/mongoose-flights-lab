f5fd452426a8efe1b47ad429b06e246c108fd22a
pohern <ohern_p@yahoo.com>
2020-12-04 13:04:17 -0600

    add departures.js route

---
+++ controllers/departures.js
@@ -0,0 +1,5 @@
+const Departure = require('../models/departure')
+
+module.export = {
+    
+}
---
+++ routes/departures.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+const departuresCtrl = require('../controllers/departures')
+
+
+module.exports = router
