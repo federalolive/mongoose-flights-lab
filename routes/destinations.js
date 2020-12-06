aa3256ccb8afc79c6cd9c8ae54a623f93e58835d
pohern <ohern_p@yahoo.com>
2020-12-05 22:03:22 -0600

    add css styling

--- routes/destinations.js
+++ routes/destinations.js
@@ -5,6 +5,6 @@ const destinationsCtrl = require('../controllers/destinations')
 router.get('/destinations/new', destinationsCtrl.new);
 router.post('/destinations', destinationsCtrl.create);
 router.post('/flights/:id/destinations', destinationsCtrl.addToDestinations);
-router.delete('/:destinationId', destinationsCtrl.delete);
+router.delete('id', destinationsCtrl.delete);
 
 module.exports = router
