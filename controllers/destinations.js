cd464011a4f2445424b752e3821f2681713d8b5d
pohern <ohern_p@yahoo.com>
2020-12-07 09:34:10 -0600

    refactor airport back in and fix destination

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -12,7 +12,7 @@ function newDestination(req, res){
     Destination.find({}, function(err, destinations) {
         res.render('destinations/new', {title: 'Add Destination', destinations})
     })
-}
+}  
 function create(req, res){
     Destination.create(req.body, function(err, destinations){
         res.redirect('/destinations/new')
